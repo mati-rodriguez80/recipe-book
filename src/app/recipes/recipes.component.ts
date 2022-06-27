78875e294b2b1f41ac69149421507fe7407621f7
mati-rodriguez80 <95318985+mati-rodriguez80@users.noreply.github.com>
2022-06-26 21:37:49 -0300

    Implement child routing

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Recipe } from './recipe.model';
+// import { Recipe } from './recipe.model';
 import { RecipeService } from './recipe.service';
 
 @Component({
@@ -10,14 +10,14 @@ import { RecipeService } from './recipe.service';
 })
 export class RecipesComponent implements OnInit {
 
-  selectedRecipe: Recipe;
+  // selectedRecipe: Recipe;
 
   constructor(private recipeService: RecipeService) { }
 
   ngOnInit(): void {
-    this.recipeService.recipeSelected.subscribe(
-      (recipe: Recipe) => { this.selectedRecipe = recipe }
-    );
+    // this.recipeService.recipeSelected.subscribe(
+    //   (recipe: Recipe) => { this.selectedRecipe = recipe }
+    // );
   }
 
 }
